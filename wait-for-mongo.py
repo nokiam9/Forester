7bfa908a23a835ffcf4986746ad69c3b1db211e2
nokiam9 <sj0225@icloud.com>
2019-11-02 08:32:22 +0800

    Feat: new py for check mongo ready

--- wait-for-mongo.py
+++ wait-for-mongo.py
@@ -1,25 +1,29 @@
 # -*- coding: utf-8 -*-
 
 import argparse
+import os
 import time
 import sys
 import subprocess
 import pymongo
 
+DEFALUT_URI = os.getenv("MONGODB_URI", default="mongodb://localhost:27017")
+DEFAULT_TIMELIMIT = 30
+
 
 def get_parse():
     parser = argparse.ArgumentParser()
 
-    parser.add_argument("uri", type=str,
-                        help="Mongodb URI, such as mongodb://localhost:27017")
-    parser.add_argument("-t", "--timeout", type=int, default=60,
-                        help="set waiting time with seconds, default is 60")
+    parser.add_argument("--uri", type=str, default=DEFALUT_URI,
+                        help=u"Mongodb URI, default get from $MONGODB_URI")
+    parser.add_argument("-t", "--timeout", type=int, default=DEFAULT_TIMELIMIT,
+                        help=u"set waiting time with seconds, default is {0}".format(DEFAULT_TIMELIMIT))
     parser.add_argument("-q", "--quiet", action="store_true",
-                        help="set quiet mode without output, default is False")
-    parser.add_argument("command", nargs="?", type=str,
-                        help="Command should be excuted after starting Mongo")
+                        help=u"set quiet mode without output, default is False")
+    parser.add_argument("command", nargs="?",
+                        help=u"Command should be excuted after starting Mongo")
     parser.add_argument("args", nargs=argparse.REMAINDER,
-                        help="Arguments for this command, if existed")
+                        help=u"Arguments for this command, if existed")
 
     return parser
 
@@ -38,7 +42,7 @@ if __name__ == '__main__':
 
     if not is_quiet:
         print(args)
-
+        
     if time_limit < 0:
         print(u"Error: parament timeout {0} is out of range.".format(
             str(time_limit)), file=sys.stderr)
@@ -60,10 +64,13 @@ if __name__ == '__main__':
             time.sleep(3)
 
     if mongo_ready:
-        if cmd is not None:
+        if cmd is not None and len(cmd) > 0:
             if not is_quiet:
                 print('Process to command line: {0}'.format([cmd] + cmd_args))
-            subprocess.call([cmd] + cmd_args)
+            try:
+                subprocess.call([cmd] + cmd_args)
+            except:
+                pass
     else:
         if not is_quiet:
             print(u"Wait-for-mongo failed and exit with -1.", file=sys.stderr)
